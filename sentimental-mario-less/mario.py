f174d9c2682df279798117317f3ba32cf89d5a6d
Uywen <uywenafrica27@gmail.com>
2023-11-09 13:55:51 +0200

    removed a few comments

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -12,7 +12,6 @@ def main():
             for j in range(1, i + 1):
              print("#", end="")   
 
-            #  for k in range(height , i , 1):
             print(" ", end="")
 
             for j in range(1, i + 1):
